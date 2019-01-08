26f3af175881e88f0aadd90fa30359e802e909a2
Youngho Jo <yhj0306@kr.ibm.com>
2019-01-08 10:52:54 +0900

    Creating Jar project

---
+++ AutoIndexing/src/com/jyh/jyh/AutoIndexing.java
@@ -0,0 +1,19 @@
+package com.jyh.jyh;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AutoIndexing {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter("test.txt"));
+		bw.write("test txt");
+		bw.write(", ");
+		bw.write("right");
+		bw.newLine();
+		bw.write("2nd line");
+		bw.close();
+		System.out.println("done");
+	}
+
+}
